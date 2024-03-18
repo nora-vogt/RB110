3ce9afed738018576ce7ce5a17dfce00d9218bf0
Nora Vogt <noravogt@protonmail.com>
2024-03-18 15:39:47 -0400

    initialized deck

---
+++ lesson_3/twentyone.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+CARDS = ('2'..'10').to_a + ['Jack', 'Queen', 'King', 'Ace']
+
+def initialize_deck
+  CARDS.product(SUITS)
+end
+
+deck = initialize_deck
