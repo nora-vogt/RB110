5cfc4ffc303c44f0c7b8595181641363b60e738d
Nora Vogt <noravogt@protonmail.com>
2024-03-19 11:37:17 -0400

    added player's turn loop

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -30,6 +30,14 @@ def initial_deal!(deck, player_hand, dealer_hand)
   end
 end
 
+def winner?(hand)
+  calculate_total(hand) == WINNING_SCORE
+end
+
+def busted?(hand)
+  calculate_total(hand) > WINNING_SCORE
+end
+
 def display_player_hand(hand)
   puts "Your cards are:"
   hand.each { |card| puts "#{card[0]} of #{card[1]}" }
@@ -39,6 +47,7 @@ def display_dealer_hand(hand)
   puts "Dealer's hand is:"
   puts "#{hand[0][0]} of #{hand[0][1]} and mystery card"
 end
+
 
 def calculate_total(hand)
   values = hand.map {|card| card.first }
@@ -70,12 +79,33 @@ loop do # start game
   initial_deal!(deck, player_hand, dealer_hand)
   blank_line
 
+  puts "Your turn!"
+  sleep 1.5
+  # need to check if dealer wins after initial deal here
+
   loop do # Player Turn
-  display_player_hand(player_hand) # player can see both cards
-  blank_line
-  display_dealer_hand(dealer_hand) # only one dealer card is visible
-  blank_line
-  break
+    system "clear"
+    display_player_hand(player_hand) # player can see both cards
+    blank_line
+    display_dealer_hand(dealer_hand) # only one dealer card is visible
+    blank_line
+
+    break if winner?(player_hand)
+    
+    puts "Would you like to hit or stay?"
+    answer = gets.chomp.downcase
+
+    deal_card!(deck, player_hand) if answer == "hit"
+
+    break if answer == "stay" || busted?(player_hand)
+  end
+
+  if winner?(player_hand)
+    puts "You win with 21 points!"
+  elsif busted?(player_hand)
+    puts "You bust! Dealer wins!"
+  else
+    puts "You chose to stay. Dealer's Turn!"
   end
 
   break
