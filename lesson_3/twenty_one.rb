f7b44368e387c5e880f3b47a473ba6bc75339ac6
Nora Vogt <noravogt@protonmail.com>
2024-03-28 09:46:06 -0400

    reordered methods

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -12,32 +12,48 @@ TEN_POINTS = 10
 ELEVEN_POINTS = 11
 GAME_WINNING_SCORE = 5
 
-def prompt(message)
-  puts "=> #{message}"
+def entered_yes?(input)
+  ['y', 'yes'].include?(input)
 end
 
-def display_blank_line
-  puts ""
+def entered_no?(input)
+  ['n', 'no'].include?(input)
 end
 
-def display_introduction
-  prompt "Welcome to Twenty-One!"
-  rules_choice = ask_for_rules
-  display_rules if entered_yes?(rules_choice)
+def within_valid_range?(string)
+  ('21'..'91').to_a.include?(string)
+end
 
-  choice = ask_customize_game
+def valid_integer?(string)
+  string.to_i.to_s == string
+end
 
-  if entered_yes?(choice)
-    score = ask_for_winning_score
-    set_game_constants(score)
-    display_custom_game_choice(score)
-  else
-    set_game_constants
-    prompt "Great, you'll stick with Twenty-One!"
-  end
+def ends_in_one?(string)
+  string.chars.last == '1'
+end
 
-  prompt "Get ready, you'll play until someone wins 5 rounds."
-  sleep 4
+def valid_winning_score?(string)
+  valid_integer?(string) && ends_in_one?(string) && within_valid_range?(string)
+end
+
+def dealer_stay?(total)
+  (DEALER_MIN_POINTS..MAX_HAND_POINTS).include?(total)
+end
+
+def busted?(total)
+  total > MAX_HAND_POINTS
+end
+
+def game_won?(scoreboard)
+  scores = scoreboard.select { |k, _| k == :user || k == :dealer }
+  scores.any? { |_, v| v == GAME_WINNING_SCORE }
+end
+
+def play_again?
+  display_blank_line
+  prompt "Would you like to play again? ('y' or 'n')"
+  answer = gets.chomp.downcase
+  ['y', 'yes'].include?(answer)
 end
 
 def ask_for_rules
@@ -47,6 +63,127 @@ def ask_for_rules
     return choice if ['yes', 'y', 'n', 'no'].include?(choice)
     prompt "Invalid response. Please enter 'Y' or 'N':"
   end
+end
+
+def ask_customize_game
+  system "clear"
+  prompt "Would you like to customize the winning score to play 'Whatever-One'?"
+  prompt "Enter 'Y' to customize, or 'N' to continue playing Twenty-One:"
+  loop do
+    input = gets.chomp.downcase
+    return input if entered_yes?(input) || entered_no?(input)
+    prompt "Invalid response. Please enter 'y' or 'n':"
+  end
+end
+
+def ask_to_start(play)
+  prompt "Press enter to start #{play == :game ? 'the game' : 'next round'}:"
+  gets.chomp
+end
+
+def ask_for_winning_score
+  system "clear"
+  prompt "Set your own winning score."
+  score = nil
+  loop do
+    prompt "Enter a number that ends in '1', between 31 and 91 (ex: 51):"
+    score = gets.chomp
+    break if valid_winning_score?(score)
+    prompt "Invalid response."
+  end
+  score.to_i
+end
+
+def ask_hit_or_stay
+  loop do
+    choice = gets.chomp.downcase
+    return choice if (['h', 'hit', 's', 'stay']).include?(choice)
+    prompt "Invalid response. Please enter 'hit' or 'stay':"
+  end
+end
+
+def set_game_score_limits(max = 21)
+  Object.const_set('DEALER_MIN_POINTS', max - 4)
+  Object.const_set('MAX_HAND_POINTS', max)
+end
+
+def format_winning_score(integer)
+  first_digit = integer.digits.last
+  "#{NUMERALS[first_digit]}-One"
+end
+
+def format_card_top(card, hidden, index)
+  if hidden && index == 0
+    "|~    |"
+  elsif card[0] == '10'
+    "|#{card[0]}   |"
+  else
+    "|#{card[0]}    |"
+  end
+end
+
+def format_card_middle(card, hidden, index)
+  "|  #{hidden && index == 0 ? '~' : card[1]}  |"
+end
+
+def format_card_bottom(card, hidden, index)
+  if hidden && index == 0
+    "|____~|"
+  elsif card[0] == '10'
+    "|___#{card[0]}|"
+  else
+    "|____#{card[0]}|"
+  end
+end
+
+def generate_display_cards!(player_info, lines)
+  hidden = player_info[:hidden_card]
+  player_info[:hand].each_with_index do |card, index|
+    lines[0] << " _____ "
+    lines[1] << format_card_top(card, hidden, index)
+    lines[2] << format_card_middle(card, hidden, index)
+    lines[3] << format_card_middle(card, hidden, index)
+    lines[4] << format_card_bottom(card, hidden, index)
+  end
+end
+
+def deal_card!(deck, hand)
+  hand << deck.shift
+end
+
+def initial_deal!(deck, user, dealer)
+  2.times do
+    deal_card!(deck, user)
+    deal_card!(deck, dealer)
+  end
+end
+
+def reveal_dealer_hidden_card!(dealer_stats)
+  dealer_stats[:hidden_card] = false
+end
+
+def update_round_number!(scoreboard)
+  scoreboard[:round] += 1
+end
+
+def update_score!(scoreboard, outcome)
+  case outcome
+  when :user_busted, :dealer then scoreboard[:dealer] += 1
+  when :dealer_busted, :user then scoreboard[:user] += 1
+  when :tie                  then scoreboard[:tie] += 1
+  end
+end
+
+def update_total!(player)
+  player[:total] = calculate_total(player[:hand])
+end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_blank_line
+  puts ""
 end
 
 def display_rules
@@ -93,17 +230,6 @@ def display_rules
   gets.chomp
 end
 
-def ask_customize_game
-  system "clear"
-  prompt "Would you like to customize the winning score to play 'Whatever-One'?"
-  prompt "Enter 'Y' to customize, or 'N' to continue playing Twenty-One:"
-  loop do
-    input = gets.chomp.downcase
-    return input if entered_yes?(input) || entered_no?(input)
-    prompt "Invalid response. Please enter 'y' or 'n':"
-  end
-end
-
 def display_custom_game_choice(score)
   system "clear"
   if score == 21
@@ -113,120 +239,24 @@ def display_custom_game_choice(score)
   end
 end
 
-def within_valid_range?(string)
-  ('21'..'91').to_a.include?(string)
-end
+def display_introduction
+  prompt "Welcome to Twenty-One!"
+  rules_choice = ask_for_rules
+  display_rules if entered_yes?(rules_choice)
 
-def valid_integer?(string)
-  string.to_i.to_s == string
-end
+  choice = ask_customize_game
 
-def ends_in_one?(string)
-  string.chars.last == '1'
-end
-
-def valid_winning_score?(string)
-  valid_integer?(string) && ends_in_one?(string) && within_valid_range?(string)
-end
-
-def format_winning_score(integer)
-  first_digit = integer.digits.last
-  "#{NUMERALS[first_digit]}-One"
-end
-
-def entered_yes?(input)
-  ['y', 'yes'].include?(input)
-end
-
-def entered_no?(input)
-  ['n', 'no'].include?(input)
-end
-
-def ask_to_start(play)
-  prompt "Press enter to start #{play == :game ? 'the game' : 'next round'}:"
-  gets.chomp
-end
-
-def ask_for_winning_score
-  system "clear"
-  prompt "Set your own winning score."
-  score = nil
-  loop do
-    prompt "Enter a number that ends in '1', between 31 and 91 (ex: 51):"
-    score = gets.chomp
-    break if valid_winning_score?(score)
-    prompt "Invalid response."
-  end
-  score.to_i
-end
-
-def set_game_constants(max = 21)
-  Object.const_set('DEALER_MIN_POINTS', max - 4)
-  Object.const_set('MAX_HAND_POINTS', max)
-end
-
-def initialize_deck
-  CARDS.product(SUITS).shuffle
-end
-
-def initialize_players(deck)
-  user_hand = []
-  dealer_hand = []
-  initial_deal!(deck, user_hand, dealer_hand)
-
-  user_total = calculate_total(user_hand)
-  dealer_total = calculate_total(dealer_hand)
-
-  user_stats = { hand: user_hand, total: user_total }
-  dealer_stats = { hand: dealer_hand, total: dealer_total, hidden_card: true }
-
-  { user: user_stats, dealer: dealer_stats }
-end
-
-def deal_card!(deck, hand)
-  hand << deck.shift
-end
-
-def initial_deal!(deck, user, dealer)
-  2.times do
-    deal_card!(deck, user)
-    deal_card!(deck, dealer)
-  end
-end
-
-def format_card_top(card, hidden, index)
-  if hidden && index == 0
-    "|~    |"
-  elsif card[0] == '10'
-    "|#{card[0]}   |"
+  if entered_yes?(choice)
+    score = ask_for_winning_score
+    set_game_score_limits(score)
+    display_custom_game_choice(score)
   else
-    "|#{card[0]}    |"
+    set_game_score_limits
+    prompt "Great, you'll stick with Twenty-One!"
   end
-end
 
-def format_card_middle(card, hidden, index)
-  "|  #{hidden && index == 0 ? '~' : card[1]}  |"
-end
-
-def format_card_bottom(card, hidden, index)
-  if hidden && index == 0
-    "|____~|"
-  elsif card[0] == '10'
-    "|___#{card[0]}|"
-  else
-    "|____#{card[0]}|"
-  end
-end
-
-def generate_display_cards!(player_info, lines)
-  hidden = player_info[:hidden_card]
-  player_info[:hand].each_with_index do |card, index|
-    lines[0] << " _____ "
-    lines[1] << format_card_top(card, hidden, index)
-    lines[2] << format_card_middle(card, hidden, index)
-    lines[3] << format_card_middle(card, hidden, index)
-    lines[4] << format_card_bottom(card, hidden, index)
-  end
+  prompt "Get ready, you'll play until someone wins 5 rounds."
+  sleep 4
 end
 
 def display_scoreboard(scoreboard)
@@ -269,11 +299,100 @@ def display_game_info(players, scoreboard)
   display_hands(players)
 end
 
-def ask_hit_or_stay
-  loop do
-    choice = gets.chomp.downcase
-    return choice if (['h', 'hit', 's', 'stay']).include?(choice)
-    prompt "Invalid response. Please enter 'hit' or 'stay':"
+def display_round_outcome(players, outcome)
+  user_total = players[:user][:total]
+  dealer_total = players[:dealer][:total]
+
+  case outcome
+  when :user_busted
+    prompt "You bust! Dealer wins."
+  when :dealer_busted
+    prompt "Dealer busts! You win this round!"
+  when :user
+    prompt "You win this round with #{user_total} points!"
+  when :dealer
+    prompt "The Dealer wins this round with #{dealer_total} points!"
+  when :tie
+    prompt "It's a tie! No points are awarded."
+  end
+end
+
+def display_end_of_round(players, scoreboard, outcome)
+  display_game_info(players, scoreboard)
+  display_round_outcome(players, outcome)
+end
+
+def display_game_winner(scoreboard)
+  winner = determine_game_winner(scoreboard)
+  display_blank_line
+  puts '*' * 80
+  case winner
+  when :user
+    prompt "You win the game! Congratulations!"
+  when :dealer
+    prompt "The Dealer wins the game! Better luck next time."
+  end
+  puts '*' * 80
+end
+
+def initialize_deck
+  CARDS.product(SUITS).shuffle
+end
+
+def initialize_players(deck)
+  user_hand = []
+  dealer_hand = []
+  initial_deal!(deck, user_hand, dealer_hand)
+
+  user_total = calculate_total(user_hand)
+  dealer_total = calculate_total(dealer_hand)
+
+  user_stats = { hand: user_hand, total: user_total }
+  dealer_stats = { hand: dealer_hand, total: dealer_total, hidden_card: true }
+
+  { user: user_stats, dealer: dealer_stats }
+end
+
+def calculate_total(hand)
+  values = hand.map(&:first)
+  sum = 0
+
+  values.each do |value|
+    sum = if value == 'A'
+            sum + ELEVEN_POINTS
+          elsif ('2'..'10').include?(value)
+            sum + value.to_i
+          else # Jack, Queen, King
+            sum + TEN_POINTS
+          end
+  end
+
+  values.count('A').times { sum -= TEN_POINTS if sum > MAX_HAND_POINTS }
+  sum
+end
+
+def determine_round_outcome(players)
+  user_total = players[:user][:total]
+  dealer_total = players[:dealer][:total]
+
+  if user_total > MAX_HAND_POINTS
+    :user_busted
+  elsif dealer_total > MAX_HAND_POINTS
+    :dealer_busted
+  elsif user_total > dealer_total
+    :user
+  elsif dealer_total > user_total
+    :dealer
+  else
+    :tie
+  end
+end
+
+def determine_game_winner(scoreboard)
+  if scoreboard[:user] == 5
+    :user
+  elsif scoreboard[:dealer] == 5
+    :dealer
   end
 end
 
@@ -295,10 +414,6 @@ def user_turn(deck, players, scoreboard)
 
     break if ['s', 'stay'].include?(choice) || busted?(user_stats[:total])
   end
-end
-
-def reveal_dealer_hidden_card!(dealer_stats)
-  dealer_stats[:hidden_card] = false
 end
 
 def dealer_turn(deck, players, scoreboard)
@@ -332,121 +447,6 @@ def play_round(players, deck, scoreboard)
   return if busted?(players[:dealer][:total])
   prompt "Dealer chose to stay."
   sleep 2
-end
-
-def calculate_total(hand)
-  values = hand.map(&:first)
-  sum = 0
-
-  values.each do |value|
-    sum = if value == 'A'
-            sum + ELEVEN_POINTS
-          elsif ('2'..'10').include?(value)
-            sum + value.to_i
-          else # Jack, Queen, King
-            sum + TEN_POINTS
-          end
-  end
-
-  values.count('A').times { sum -= TEN_POINTS if sum > MAX_HAND_POINTS }
-  sum
-end
-
-def update_total!(player)
-  player[:total] = calculate_total(player[:hand])
-end
-
-def determine_round_outcome(players)
-  user_total = players[:user][:total]
-  dealer_total = players[:dealer][:total]
-
-  if user_total > MAX_HAND_POINTS
-    :user_busted
-  elsif dealer_total > MAX_HAND_POINTS
-    :dealer_busted
-  elsif user_total > dealer_total
-    :user
-  elsif dealer_total > user_total
-    :dealer
-  else
-    :tie
-  end
-end
-
-def update_score!(scoreboard, outcome)
-  case outcome
-  when :user_busted, :dealer then scoreboard[:dealer] += 1
-  when :dealer_busted, :user then scoreboard[:user] += 1
-  when :tie                  then scoreboard[:tie] += 1
-  end
-end
-
-def update_round_number!(scoreboard)
-  scoreboard[:round] += 1
-end
-
-def display_round_outcome(players, outcome)
-  user_total = players[:user][:total]
-  dealer_total = players[:dealer][:total]
-
-  case outcome
-  when :user_busted
-    prompt "You bust! Dealer wins."
-  when :dealer_busted
-    prompt "Dealer bust! You win!"
-  when :user
-    prompt "You win this round with #{user_total} points!"
-  when :dealer
-    prompt "The Dealer wins this round with #{dealer_total} points!"
-  when :tie
-    prompt "It's a tie!"
-  end
-end
-
-def display_end_of_round(players, scoreboard, outcome)
-  display_game_info(players, scoreboard)
-  display_round_outcome(players, outcome)
-end
-
-def determine_game_winner(scoreboard)
-  if scoreboard[:user] == 5
-    :user
-  elsif scoreboard[:dealer] == 5
-    :dealer
-  end
-end
-
-def display_game_winner(scoreboard)
-  winner = determine_game_winner(scoreboard)
-  display_blank_line
-  puts '*' * 80
-  case winner
-  when :user
-    prompt "You win the game! Congratulations!"
-  when :dealer
-    prompt "The Dealer wins the game! Better luck next time."
-  end
-  puts '*' * 80
-end
-
-def dealer_stay?(total)
-  (DEALER_MIN_POINTS..MAX_HAND_POINTS).include?(total)
-end
-
-def busted?(total)
-  total > MAX_HAND_POINTS
-end
-
-def game_won?(scoreboard)
-  scores = scoreboard.select { |k, _| k == :user || k == :dealer }
-  scores.any? { |_, v| v == GAME_WINNING_SCORE }
-end
-
-def play_again?
-  display_blank_line
-  prompt "Would you like to play again? ('y' or 'n')"
-  answer = gets.chomp.downcase
-  ['y', 'yes'].include?(answer)
 end
 
 loop do
